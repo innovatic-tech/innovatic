0f79b5583d1c140becb3cfaa141046b0f7f11f1e
innovatic <innovaticlaconcordia@gmail.com>
2026-03-19 15:26:19 -0500

    Update estilo.js

--- estilo.js
+++ estilo.js
@@ -56,7 +56,7 @@ form.addEventListener("submit", async function(e) {
     const data = new FormData(form);
 
     try {
-        const response = await fetch("https://script.google.com/macros/s/AKfycbxNqT3r_Haf3EbZSXWeAh-wYWCpzpQQntyQqAEWManOCbRdv5_LhKHQ58ohbIcm6Q-B/exec", {
+        const response = await fetch("https://formspree.io/f/xkoqybrl", {
             method: "POST",
             body: data,
             headers: {
